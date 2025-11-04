08ae157e1a499e4557ac070159ddb9e779d84207
Kike <5785235@alu.murciaeduca.es>
2025-11-04 09:52:51 +0100

    Ruta Actualizada

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,44 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Pantalla principal';
 });
+
+Route::get('/login', function () {
+    return 'Login usuario';
+});
+
+Route::get('/logout', function () {
+    return 'Logout usuario';
+});
+
+Route::prefix('/proyectos')->group(function () {
+    Route::get('/', function () {
+    return 'Listado proyectos';
+   });
+
+   Route::get('/show/{id}', function ($id) {
+    return 'Vista detalle proyecto '.$id;
+   })->where(array('id' => '[0-9]+')) ;
+
+  Route::get('/create', function () {
+    return 'Añadir proyecto';
+  });
+
+  Route::get('/edit/{id}', function ($id) {
+    return 'Modificar proyecto '.$id;
+  })->where(array('id' => '[0-9]+'))  ;
+
+
+});
+
+Route::get('/perfil/{id?}', function ($id=null) {
+    if($id){
+	$mensaje= 'Visualizar el currículo de '.$id;
+    }else{
+    	$mensaje= 'Visualizar el currículo propio';
+    }
+   return $mensaje;
+})->where(array('id' => '[0-9]+'))  ;
+
+
