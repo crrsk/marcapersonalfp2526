f7dcb8097559b1199969be99f748a93cc71d4738
Kike <5785235@alu.murciaeduca.es>
2025-11-11 09:15:25 +0100

    vista Home integrada en dopetrope

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,20 +2,11 @@
 
 @section('menu')
     @parent
-    <p>¡¡ Hola {{ $nombre ?? '' }} !! </p>
+    <li>Opcion adicional</li>
 @endsection
 @section('content')
-  <ul>
-        @if (count($users) === 1)
-            <li>Solo hay un usuario!</li>
-        @elseif (count($users) > 1)
-        <li> Hay muchos usuarios! </li>
-            <ul>@include("users.usersList",["users"=>$users])</ul>
-        @else
-        <li>No hay ningún usuario :(</li>
-        @endif
-
-   </ul>
+    <h2>Marca Personal FP</h2>
+    <p>Pagina principal</p>
 @endsection
 
 
