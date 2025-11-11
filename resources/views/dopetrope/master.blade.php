87eb28590046d316de5bb138d99dab459d22c27c
Diego <2863238@alu.murciaeduca.es>
2025-11-11 09:15:29 +0100

    Vista home integrada en Dopetrope

--- resources/views/dopetrope/master.blade.php
+++ resources/views/dopetrope/master.blade.php
@@ -27,6 +27,7 @@
 					<!-- Nav -->
 						<nav id="nav">
 							<ul>
+@section('menu')
 								<li class="current"><a href="index.html">Home</a></li>
 								<li>
 									<a href="#">Dropdown</a>
@@ -50,14 +51,14 @@
 								<li><a href="left-sidebar.html">Left Sidebar</a></li>
 								<li><a href="right-sidebar.html">Right Sidebar</a></li>
 								<li><a href="no-sidebar.html">No Sidebar</a></li>
+@show
 							</ul>
 						</nav>
 
 					<!-- Banner -->
 						<section id="banner">
 							<header>
-								<h2>Howdy. This is Dopetrope.</h2>
-								<p>A responsive template by HTML5 UP</p>
+                                @yield('content')
 							</header>
 						</section>
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,10 @@
 
     @section('menu')
         @parent
-        <p>¡Hola {{ $nombre ?? 'colega' }} !</p>
+        <li>Opcion adicional</li>
     @endsection
 
     @section('content')
-        <ul>
-        @if (count($users) === 1)
-            <li>Solo hay un usuario!</li>
-        @elseif (count($users) > 1)
-            <li>Hay muchos usuarios!</li>
-            @include('users.usersLista', ['users' => $users])
-        @else
-            <li>No hay ningún usuario :(</li>
-        @endif
-        </ul>
+    <h2>Marca Personal F.P.</h2>
+	<p>Página principal</p>
     @endsection
-
-
-
-
