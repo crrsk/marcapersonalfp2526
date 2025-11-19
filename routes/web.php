b3479de86918db7d7cd7b53bee8419432a53d948
Alberto Sierra <alberto.sierra@murciaeduca.es>
2025-11-19 09:50:14 +0100

    Ejercicio contrladores hasta getShow

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProyectosController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [HomeController::class, 'getHome']);
 
 // ----------------------------------------
 Route::get('login', function () {
@@ -17,21 +17,13 @@ Route::get('logout', function () {
 
 // ----------------------------------------
 Route::prefix('proyectos')->group(function () {
-    Route::get('/', function () {
-        return view('proyectos.index');
-    });
+    Route::get('/', [ProyectosController::class, 'getIndex']);
 
-    Route::get('create', function () {
-        return view('proyectos.create');
-    });
+    Route::get('create', [ProyectosController::class, 'getCreate']);
 
-    Route::get('/show/{id}', function ($id) {
-        return view('proyectos.show', array('id'=>$id));
-    }) -> where('id', '[0-9]+');
+    Route::get('/show/{id}', [ProyectosController::class, 'getShow'])->where('id', '[0-9]+');
 
-    Route::get('/edit/{id}', function ($id) {
-        return view('proyectos.edit', array('id'=>$id));
-    }) -> where('id', '[0-9]+');
+    Route::get('/edit/{id}', [ProyectosController::class, 'getEdit'])->where('id', '[0-9]+');
 });
 
 
