266aa561da023937e4e3a46ed9849ac0997c59b0
Alberto Sierra <alberto.sierra@murciaeduca.es>
2026-01-26 08:30:28 +0100

    Creación de relación 1 a 1 User-Curriculo

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function curriculo()
+    {
+        return $this->hasOne(Curriculo::class);
+    }
 }
